9730c23249bf246a3e9704992ade55d7565b5da3
ugurduyar <ismailugurduyar@gmail.com>
2020-06-06 10:59:24 +0300

    img and name of api sorted

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const search = document.getElementById("search"),
   submit = document.getElementById("submit"),
   random = document.getElementById("random"),
   mealsEl = document.getElementById("meals"),
-  resultHeading = document.getElementById("results-heading"),
+  resultHeading = document.getElementById("result-heading"),
   single_mealEl = document.getElementById("single-meal");
 
 // Search meal and fetch from API
@@ -21,7 +21,28 @@ function searchMeal(e) {
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
+        resultHeading.innerHTML = `<h2>Search Results For '${term}': </h2>`;
+
+        if (data.meals === null) {
+          resultHeading.innerHTML = `<p>There are no search results. Try again! <p>`;
+        } else {
+          mealsEl.innerHTML = data.meals
+            .map(
+              (meal) => `
+            <div class="meal">
+             <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+             <div class="meal-info" data-mealID="${meal.idMeal}" >
+                <h3>${meal.strMeal}</h3>
+             </div>
+            </div>
+            `
+            )
+            .join("");
+        }
       });
+
+    // Clear search text
+    search.value = "";
   } else {
     alert("Please enter a search term");
   }
