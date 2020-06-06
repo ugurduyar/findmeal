92e42ae581d7b7410538fe243695ae7f898f503f
ugurduyar <ismailugurduyar@gmail.com>
2020-06-06 12:40:57 +0300

    fetch ingredients and add them to DOM

--- script.js
+++ script.js
@@ -48,5 +48,43 @@ function searchMeal(e) {
   }
 }
 
+// Fetch meal by id
+function getMealById(mealID) {
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
+    .then((res) => res.json())
+    .then((data) => {
+      const meal = data.meals[0];
+      addMealToDOM(meal);
+    });
+}
+
+// Add meal to DOM
+function addMealToDOM(meal) {
+    const ingredients = []
+
+    for(let i = 1 ; i <= 20; i++) {
+        if(meal.[`strIngredient${i}`]) {
+            ingredients.push(`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]} `);
+        } else {
+            break;
+        }
+    }
+}
+
 // Event listeners
 submit.addEventListener("submit", searchMeal);
+
+mealsEl.addEventListener("click", (e) => {
+  const mealInfo = e.path.find((item) => {
+    if (item.classList) {
+      return item.classList.contains("meal-info");
+    } else {
+      return false;
+    }
+  });
+
+  if (mealInfo) {
+    const mealID = mealInfo.getAttribute("data-mealid");
+    getMealById(mealID);
+  }
+});
