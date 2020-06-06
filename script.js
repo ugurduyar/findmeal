749cae1ee03ab7d73c2598ff095d9a489f9b2386
ugurduyar <ismailugurduyar@gmail.com>
2020-06-06 10:43:28 +0300

    term check - api fetch

--- script.js
+++ script.js
@@ -4,3 +4,28 @@ const search = document.getElementById("search"),
   mealsEl = document.getElementById("meals"),
   resultHeading = document.getElementById("results-heading"),
   single_mealEl = document.getElementById("single-meal");
+
+// Search meal and fetch from API
+function searchMeal(e) {
+  e.preventDefault();
+
+  // Clear single meal
+  single_mealEl.innerHTML = "";
+
+  // Get search term
+  const term = search.value;
+
+  // Check for empty
+  if (term.trim()) {
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  } else {
+    alert("Please enter a search term");
+  }
+}
+
+// Event listeners
+submit.addEventListener("submit", searchMeal);
